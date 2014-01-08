e36a56c3b7945c7dd960e076908a325d00a6e852
Jade Meskill and Roy van de Water <dev+jade+roy@integrumtech.com>
2014-01-07 17:22:00 -0700

    Add Rails boilerplate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WrenchClock::Application.config.secret_key_base = '819f2a7987e040b59d66504d425811793e0b0aff763ceb74b03b11a5f182aaa67ade10a580904f2d726a5ee1413fab25b23260a5676c7d42df55b7565b5131f6'
